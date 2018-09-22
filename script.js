e301090aea879be6b4c8b2385661938af5ca3a65
makito <makitodev@outlook.com>
2018-09-21 20:18:52 -0500

    changes on scriptsjs

--- script.js
+++ script.js
@@ -1,48 +1,39 @@
 var palette = [];
-const S = 0.5;
-const L = 0.5;
-const DIF_TONO = 360 / 5;
+const numColors = 5
+const gap_hue = 360 / numColors;
 
 function randomPalette(){
+    const saturation = 0.5;
+    const lightness = 0.5;
     posIni = Math.floor(Math.random() * 360);
-
     palette = [];
-    
     for(let i = 0; i < 5; i++) {
-
-        var rgb = hslToRgb(posIni / 360, S, L);
+        var rgb = hslToRgb(posIni / 360, saturation, lightness);
         palette.push(rgbToStringColor(rgb[0], rgb[1], rgb[2]));
-        posIni = (posIni + DIF_TONO) % 360;
+        posIni = (posIni + gap_hue) % 360;
     }
-    mostrarColores();
+    showColors();
     generateRules();
 }
 
-
-
 function generateRules(){
-
     var  rules = [];
-    
     rules.push(".website-background{ color: " + palette[0] + ";}");
     rules.push(".element-text{ color: " + palette[1] + ";}");
     rules.push(".element-border{ border-color:" + palette[2] + ";}");
     rules.push(".element-background{ background-color: " + palette[3] + ";}");
     rules.push(".header{ color: " + palette[4] + ";}");
-    
     $("#css-rules").val(rules.join("\n\n"));
-    
 }
 
-function mostrarColores()
+function showColors()
 {
     for(let i = 1; i <= 5; i++) {
         $("#color" + i).attr("style", "background-color: " + palette[i-1] + ";");
     }
 }
 
-function cleanPalette(){
-    console.log( $(document)[0].styleSheets[0].cssRules.length);
+function cleanColors(){
     palette = [];
     for (let index = 1; index <= 5; index++) {
         $("#color"+ index).removeAttr("style");
